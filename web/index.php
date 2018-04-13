b601f979d805595de6ec8596ba3c64316fddbd7a
Michael Libman <lcdstudio@yandex.ru>
2018-04-13 23:43:15 +0300

    Add web

---
+++ web/index.php
@@ -0,0 +1,11 @@
+﻿<!DOCTYPE html>
+<html xmlns:og="http://ogp.me/ns#" prefix="og: http://ogp.me/ns#">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>T2Mapping v2.0</title>
+</head>
+<body>
+	<h1>Comming soon</h1>
+</body>
+</html>
