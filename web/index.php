6ee5f6c7717db1d48cd71696f5e385922e531e34
Michael Libman <lcdstudio@yandex.ru>
2018-12-13 22:07:33 +0300

    yandex verification

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 ﻿<!DOCTYPE html>
 <html xmlns:og="http://ogp.me/ns#" prefix="og: http://ogp.me/ns#">
 <head>
+	<meta name="yandex-verification" content="9775afbe88eee2a5" />
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script type="text/javascript" src="https://yastatic.net/jquery/3.1.1/jquery.min.js"></script>
